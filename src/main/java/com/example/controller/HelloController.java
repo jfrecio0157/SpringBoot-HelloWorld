2762635d67ca61df149f6f96d4b32ca9d806da64
Paco Recio <jfrecio@gmail.com>
2025-11-17 12:53:57 +0100

    Inclusion Dockerfile

--- src/main/java/com/example/controller/HelloController.java
+++ src/main/java/com/example/controller/HelloController.java
@@ -5,8 +5,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    /**
+     * Return a hello world message.
+     *
+     * @return a hello world message
+     */
+
     @GetMapping("/")
     public String hello() {
+
         return "Hello World";
     }
 }
